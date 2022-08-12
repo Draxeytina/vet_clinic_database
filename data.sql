d5c03f001358ac31346f5117c1cf4b2e192dd7da
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-08-12 20:20:42 +0200

    Add join table, update data, make queries

--- data.sql
+++ data.sql
@@ -32,3 +32,36 @@ BEGIN;
 UPDATE animals SET species_id = 'Digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'Pokemon' WHERE species_id IS null;
 COMMIT;
+
+-- ################################# DAY 4 ######################################
+
+INSERT into vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23'), ('Maisy Smith', 26, '2019-01-17'), ('Stephanie Mendez', 64, '1981-05-04'), ('Jack Harkness', 38, '2008-06-08');
+
+BEGIN;
+INSERT into specializations (vet_id, species_id) SELECT vets.id, species.id FROM vets, species WHERE vets.name = 'William Tatcher' AND species.name = 'Pokemon';
+INSERT INTO specializations (vet_id, species_id) SELECT vets.id, species.id FROM vets, species WHERE vets.name = 'Stephanie Mendez' AND species.name IN ('Digimon','Pokemon');
+INSERT INTO specializations (vet_id, species_id) SELECT vets.id, species.id FROM vets, species WHERE vets.name = 'Jack Harkness' AND species.name = 'Digimon';
+COMMIT;
+
+BEGIN;
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-05-24' FROM animals, vets WHERE animals.name = 'Agumon' AND vets.name = 'William Tatcher';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-07-22' FROM animals, vets WHERE animals.name = 'Agumon' AND vets.name = 'Stephanie Mendez';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2021-02-02' FROM animals, vets WHERE animals.name = 'Gabumon' AND vets.name = 'Jack Harkness';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-01-05' FROM animals, vets WHERE animals.name = 'Pikachu' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-03-08' FROM animals, vets WHERE animals.name = 'Pikachu' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-05-14' FROM animals, vets WHERE animals.name = 'Pikachu' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2021-05-04' FROM animals, vets WHERE animals.name = 'Devimon' AND vets.name = 'Stephanie Mendez';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2021-02-24' FROM animals, vets WHERE animals.name = 'Charmander' AND vets.name = 'Jack Harkness';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2019-12-21' FROM animals, vets WHERE animals.name = 'Plantmon' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-08-10' FROM animals, vets WHERE animals.name = 'Plantmon' AND vets.name = 'William Tatcher';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2021-04-07' FROM animals, vets WHERE animals.name = 'Plantmon' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2019-09-29' FROM animals, vets WHERE animals.name = 'Squirtle' AND vets.name = 'Stephanie Mendez';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-10-03' FROM animals, vets WHERE animals.name = 'Angemon' AND vets.name = 'Jack Harkness';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-11-04' FROM animals, vets WHERE animals.name = 'Angemon' AND vets.name = 'Jack Harkness';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2019-01-24' FROM animals, vets WHERE animals.name = 'Boarmon' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2019-05-15' FROM animals, vets WHERE animals.name = 'Boarmon' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-02-27' FROM animals, vets WHERE animals.name = 'Boarmon' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-08-03' FROM animals, vets WHERE animals.name = 'Boarmon' AND vets.name = 'Maisy Smith';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2020-05-24' FROM animals, vets WHERE animals.name = 'Blossom' AND vets.name = 'Stephanie Mendez';
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT animals.id, vets.id, '2021-01-11' FROM animals, vets WHERE animals.name = 'Blossom' AND vets.name = 'William Tatcher';
+COMMIT;
--- queries.sql
+++ queries.sql
@@ -75,3 +75,15 @@ SELECT species.name, COUNT(animals.name) FROM animals INNER JOIN species ON spec
 SELECT owners.full_name, species.name, animals.name FROM animals INNER JOIN species ON species.id = animals.species_id INNER JOIN owners ON owners.id = animals.owner_id WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
 SELECT owners.full_name, animals.name FROM animals JOIN owners ON owners.id = animals.owner_id WHERE owners.full_name = 'Dean Winchester' AND escape_attempts = 0;
 SELECT owners.full_name as Name, COUNT(animals.name) FROM animals JOIN owners ON owners.id = animals.owner_id GROUP BY owners.full_name ORDER BY COUNT(animals.id) DESC LIMIT 1;
+
+-- ################################# DAY 4 ######################################
+
+SELECT x.name as "Vet Name", a.name as "Animal", v.date_of_visit as "Last Date of Visit" FROM visits v INNER JOIN animals a ON a.id = v.animal_id INNER JOIN vets x ON x.id = v.vet_id WHERE x.name = 'William Tatcher' ORDER BY v.date_of_visit DESC LIMIT 1;
+SELECT vets.name, COUNT(visits.animal_id) as number_of_animals FROM visits INNER JOIN animals ON animals.id = visits.animal_id INNER JOIN vets ON vets.id = visits.vet_id WHERE vets.name = 'Stephanie Mendez' GROUP BY vets.name;
+SELECT vets.name as vet, species.name as speciality FROM vets LEFT OUTER JOIN specializations ON specializations.vet_id = vets.id LEFT JOIN species ON species.id = specializations.species_id;
+SELECT animals.name, vets.name, visits.date_of_visit FROM visits INNER JOIN animals ON animals.id = visits.animal_id INNER JOIN vets ON vets.id = visits.vet_id WHERE vets.name = 'Stephanie Mendez' AND visits.date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
+SELECT a.name as animal_name, COUNT(v.date_of_visit) AS number_of_visits FROM visits v INNER JOIN animals a ON a.id = v.animal_id INNER JOIN vets x ON x.id = v.vet_id GROUP BY a.id ORDER BY COUNT(v.date_of_visit) DESC LIMIT 1;
+SELECT x.name as vet, a.name as animal, v.date_of_visit as first_visit_date FROM visits v INNER JOIN animals a ON a.id = v.animal_id INNER JOIN vets x ON x.id = v.vet_id WHERE x.name = 'Maisy Smith' ORDER BY v.date_of_visit ASC LIMIT 1;
+SELECT a.*, x.*, v.date_of_visit FROM visits v FULL OUTER JOIN animals a ON a.id = v.animal_id FULL OUTER JOIN vets x ON x.id = v.vet_id ORDER BY v.date_of_visit DESC;
+SELECT COUNT(v.date_of_visit) as "unspecializing vets visits" FROM visits v INNER JOIN vets x ON x.id = v.vet_id INNER JOIN animals a ON a.id = v.animal_id WHERE a.species_id NOT IN (SELECT COALESCE(s.species_id, 0)	FROM vets x LEFT OUTER JOIN specializations s ON s.vet_id = x.id WHERE x.id = v.vet_id);
+SELECT s.name as "Maisy should specialize in:" FROM visits v INNER JOIN vets x ON x.id = v.vet_id INNER JOIN animals a ON a.id = v.animal_id INNER JOIN species s ON s.id = a.species_id WHERE x.name = 'Maisy Smith' GROUP BY x.name, s.name ORDER BY COUNT(a.species_id) DESC LIMIT 1;
